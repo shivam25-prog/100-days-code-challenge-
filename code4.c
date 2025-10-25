f4179b0a0b0ff5b7ccba70ee69243660b893ceeb
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2025-10-25 23:03:11 +0530

    code4.c

--- code4.c
+++ code4.c
@@ -1,3 +1,4 @@
+// Write a program to calculate the area and circumference of a circle given its radius.
 #include <stdio.h>
 #define PI 3.14159
 
@@ -18,3 +19,4 @@ int main() {
 
     return 0;
 }
+
